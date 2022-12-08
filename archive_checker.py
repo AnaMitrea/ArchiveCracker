4b32ae980f55366691a6c9f51c7e6e93941044a8
Ana Maria Mitrea <anamitrea27@gmail.com>
2022-12-08 23:05:18 +0200

    Moved Files, documentation

--- archive_checker.py
+++ archive_checker.py
@@ -3,6 +3,11 @@ from zipfile import BadZipFile, ZipFile, is_zipfile
 
 
 def check_archive(path: str):
+    """
+    Checks path from given input if the file exists and if the file is a zip
+    :param path:    file path
+    :return: None
+    """
     try:
         if not os.path.exists(path):
             raise FileNotFoundError
@@ -19,10 +24,17 @@ def check_archive(path: str):
 
 
 def check_pwd(path: str, pwd: str):
+    """
+    Checks if given string password can be used to open (extract) given zip to 'extracted_archive' directory. If zip
+    cannot be extracted, then function throws ZIP Exception
+    :param path: Path to Zip File
+    :param pwd: String Password
+    :return: True is password was correct, None otherwise
+    """
     try:
         pwd = pwd.strip()
         with ZipFile(file=path, mode='r') as archive:
-            archive.extractall('extracted_archive', pwd=bytes(pwd, 'utf-8'))
+            archive.extractall('files/extracted_archive', pwd=bytes(pwd, 'utf-8'))
         return True
     except Exception as e:
         # print(f'{pwd}')
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from pwd_generator import crack_password
 
 if __name__ == "__main__":
-    # crack_password("archive_to_crack.zip")  # aaa1
-    crack_password("archive_to_crack2.zip")  # baa
+    # crack_password("files/archive_to_crack.zip")  # aaa1
+    crack_password("files/archive_to_crack2.zip")  # baa
