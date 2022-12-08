f3924adcb0e392956492e2995495bbc5d61ce0b2
anstoian <ana-maria.mitrea@endava.com>
2022-12-08 14:42:44 +0200

    Moved files, wrote python documentation for written functions

--- archive_checker.py
+++ archive_checker.py
@@ -2,7 +2,12 @@ import os
 from zipfile import BadZipFile, ZipFile, is_zipfile
 
 
-def check_archive(path: str):
+def check_archive(path: str) -> None:
+    """
+    Checks path from given input if the file exists and if the file is a zip
+    :param path:    file path
+    :return: None
+    """
     try:
         if not os.path.exists(path):
             raise FileNotFoundError
@@ -19,6 +24,13 @@ def check_archive(path: str):
 
 
 def check_pwd(path: str, pwd: str):
+    """
+    Checks if given string password can be used to open (extract) given zip to 'extracted_archive' directory. If zip
+    cannot be extracted, then function throws ZIP Exception
+    :param path: Path to Zip File
+    :param pwd: String Password
+    :return: True is password was correct, None otherwise
+    """
     try:
         pwd = pwd.strip()
         with ZipFile(file=path, mode='r') as archive:
